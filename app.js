a8cd86ad1650245446bf85ff606d5618d94080fb
petrakoww <vasil4y@abv.bg>
2022-01-27 14:07:34 +0200

    Reuse functions

--- app.js
+++ app.js
@@ -3,264 +3,273 @@ function slider(sliderContainer) {
     return;
   }
   const slider = sliderContainer.querySelector(".carousel-slide");
-  const elementsInCarousel = sliderContainer.querySelectorAll(
+  const slidesInCarousel = sliderContainer.querySelectorAll(
     ".carousel-slide img"
   );
 
-  const prevButton = document.createElement("button");
-  const nextButton = document.createElement("button");
-
-  let counter = 0;
-  let imagesSizesArray = [];
+  let currentSlideIndex = 0;
+  let slideWidths = [];
   let currentImageSize = 0;
   let transitionSize = 0;
-  let numberOfImagesInCarousel = elementsInCarousel.length;
-  let fullWidth = 0;
+  let numberOfElementsInCarousel = slidesInCarousel.length;
+  let startPosOfLastElement = 0;
+  let start, currPosition, navigationDots;
+  let dot, moveToSlide;;
 
-  let start, currPosition;
+  //get sizes of images in array to use currentImageSize with [counter] + full width of carousel
+  slidesInCarousel.forEach((slide, index) => {
+    slideWidths.push(slide.clientWidth);
+    if (index < numberOfElementsInCarousel - 1) {
+      startPosOfLastElement += slide.clientWidth;
+    }
+  });
 
-  //get sizes of images in array to use currentImageSize with [counter]
-  for (let i = 0; i < numberOfImagesInCarousel; i++) {
-    imagesSizesArray.push(elementsInCarousel[i].clientWidth);
+  function goToSlide(slideToGo){
+    
+    transitionSize = 0;
+    slidesInCarousel.forEach((slide, index) => {
+      if (index < slideToGo) {
+        transitionSize += slide.clientWidth;
+      }
+      // console.log(transitionSize);
+    });
+    slider.style.transform = `translateX(${-transitionSize}px)`;
+
+    navigationDots.forEach((dot) => {
+      if (dot.classList.contains("dot-red")) {
+        dot.classList.remove("dot-red");
+      }
+    });
+    navigationDots[slideToGo].classList.add("dot-red");
+    currentSlideIndex = slideToGo;
+    moveToSlide = slideToGo;
+    console.log(currentSlideIndex, numberOfElementsInCarousel);
   }
 
-  //full width of carousel
-  for (let i = 0; i < numberOfImagesInCarousel; i++) {
-    fullWidth += imagesSizesArray[i];
-  }
-  //remove last element size because it gives us full size. we need last pic's beginning not end
-  fullWidth = fullWidth - imagesSizesArray[numberOfImagesInCarousel - 1];
+  function createNavDots() {
+    const dotsContainer = document.createElement("div");
+    dotsContainer.classList.add("alignCenter");
+    sliderContainer.insertAdjacentElement("beforeend", dotsContainer);
 
-  let activeButton;
-  function dots(bool) {
-    if (bool) {
-      //create dots
-      const circleDiv = document.createElement("div");
-      circleDiv.style.textAlign = "center";
-      sliderContainer.insertAdjacentElement("beforeend", circleDiv);
+    slideWidths.forEach((el, index) => {
+      dot = document.createElement("button");
+      dot.className = "dot";
 
-      let dot;
-      for (let i = 0; i < numberOfImagesInCarousel; i++) {
-        dot = document.createElement("button");
-        dot.className = "dot";
-        dot.id = i;
-        circleDiv.appendChild(dot);
+      dot.id = index;
+      dotsContainer.appendChild(dot);
+    });
+
+    //active button
+    navigationDots = sliderContainer.querySelectorAll(".dot");
+
+    dotsContainer.addEventListener("click", (e) => {
+      transitionSize = 0;
+
+      let { id } = e.target;
+
+      if (id) {
+        goToSlide(id);
       }
 
-      //active button
-      activeButton = sliderContainer.querySelectorAll(".dot");
-      activeButton[0].style.backgroundColor = "red";
-
-      circleDiv.addEventListener("click", (e) => {
-        transitionSize = 0;
-
-        const { id } = e.target;
-        //console.log(e.target);
-
-        if (id) {
-          for (let i = 0; i < id; i++) {
-            transitionSize += imagesSizesArray[i];
+      currentSlideIndex = id;
+      //sets inactive buttons grey and checks if e.target is accurate
+      if (e.target.classList.contains("dot")) {
+        navigationDots.forEach((dot) => {
+          if (dot.classList.contains("dot-red")) {
+            dot.classList.remove("dot-red");
           }
-          slider.style.transform = "translateX(" + -transitionSize + "px)";
-        }
+        });
+        navigationDots[currentSlideIndex].classList.add("dot-red");
+      }
+    });
+  }
+  
+  function moveSlide(direction) {
+    if (direction === "next") {
+      if (currentSlideIndex >= numberOfElementsInCarousel-1) {
+        goToSlide(0)
+      }else{
 
-        counter = id;
-        //sets inactive buttons grey and checks if e.target is accurate
-        if (e.target.className == "dot") {
-          for (let i = 0; i <= numberOfImagesInCarousel - 1; i++) {
-            activeButton[i].style.backgroundColor = "#bbb";
-          }
-        }
+        moveToSlide++;
+        goToSlide(moveToSlide)
+      }
+    } else if (direction === "prev") {
+      if (currentSlideIndex <= 0) {
+        goToSlide(numberOfElementsInCarousel-1)
+      }else{moveToSlide--;
+      goToSlide(moveToSlide)
+      console.log(moveToSlide);}
 
-        //sets active buttons red
-        if (counter) {
-          activeButton[counter].style.backgroundColor = "red";
-        }
-      });
+
+      
     }
   }
-  function buttons(bool) {
-    if (bool === true) {
-      //create prev and next button dynamically
-      //prev
-      prevButton.textContent = "Prev";
-      sliderContainer.insertAdjacentElement("beforebegin", prevButton);
+  function buttons() {
+    //create prev and next button dynamically
+    //prev
+    const prevButton = document.createElement("button");
+    prevButton.textContent = "Prev";
+    sliderContainer.insertAdjacentElement("beforebegin", prevButton);
 
-      //next
-      nextButton.textContent = "Next";
-      sliderContainer.insertAdjacentElement("afterend", nextButton);
+    //next
+    const nextButton = document.createElement("button");
+    nextButton.textContent = "Next";
+    sliderContainer.insertAdjacentElement("afterend", nextButton);
 
-      nextButton.addEventListener("click", () => {
-        currentImageSize = imagesSizesArray[counter];
-        counter++;
+    nextButton.addEventListener("click", () => {
+      moveSlide("next");
+    });
 
-        transitionSize += currentImageSize;
-        if (counter >= numberOfImagesInCarousel) {
-          counter = 0;
-          transitionSize = 0;
-        }
+    prevButton.addEventListener("click", () => {
+      moveSlide("prev");
+    });
+  }
+  function arrowKeys() {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "ArrowLeft") {
+        moveSlide("prev");
+      } else if (e.key === "ArrowRight") {
+        moveSlide("next");
+      }
+    });
+  }
+  function draggable() {
+    let pressed = false;
 
-        slider.style.transform = "translateX(" + -transitionSize + "px)";
+    slider.addEventListener("click", sliderClick);
+    slider.addEventListener("mousedown", sliderMouseDown);
+    slider.addEventListener("mouseenter", sliderMouseEnter);
 
-        if (counter) {
-          activeButton[counter].style.backgroundColor = "red";
-          activeButton[counter - 1].style.backgroundColor = "#bbb";
-        } else if (counter == 0) {
-          activeButton[0].style.backgroundColor = "red";
-          //sets last button to inactive
-          activeButton[numberOfImagesInCarousel - 1].style.backgroundColor =
-            "#bbb";
-        }
-      });
+    function sliderMouseEnter() {
+      slider.style.cursor = "grab";
+    }
 
-      prevButton.addEventListener("click", () => {
-        currentImageSize = imagesSizesArray[counter];
-        counter--;
-        transitionSize -= currentImageSize;
+    function sliderMouseDown(e) {
+      pressed = true;
+      slider.style.cursor = "grabbing";
+      start = e.offsetX;
+      slider.addEventListener("mousemove", sliderMouseMove);
+    }
 
-        if (counter < 0) {
-          counter = numberOfImagesInCarousel - 1;
-          transitionSize = fullWidth;
-        }
-        slider.style.transform = "translateX(" + -transitionSize + "px)";
+    function sliderMouseMove(e) {
+      pressed = true;
+      slider.style.cursor = "grabbing";
+      e.preventDefault();
+      currPosition = e.offsetX;
+      if (start > 250) {
+        slider.style.transform =
+          "translateX(" +
+          (-slideWidths[currentSlideIndex] * currentSlideIndex -
+            250 -
+            (499 - e.clientX)) +
+          "px)";
+      } else {
+        slider.style.transform =
+          "translateX(" +
+          (-slideWidths[currentSlideIndex] * currentSlideIndex +
+            250 -
+            (499 - e.clientX)) +
+          "px)";
+      }
 
-        activeButton[counter].style.backgroundColor = "red";
-        if (activeButton[counter].id != 0) {
-          activeButton[0].style.backgroundColor = "#bbb";
-        }
-        if (counter < numberOfImagesInCarousel - 1) {
-          activeButton[counter + 1].style.backgroundColor = "#bbb";
-        }
-      });
+      // slider.style.transform = "translateX(" + ((-imagesSizesArray[counter]*counter)-e.clientX-252+500) + "px)";
+      // slider.style.transform = "translateX(" + (asd-252-500) + "px)";
+      console.log(
+        -slideWidths[currentSlideIndex] * currentSlideIndex -
+          250 -
+          (499 - e.clientX)
+      );
+      console.log("Start ", start, " End ", currPosition);
+      //   if (start > currPosition) {
+      //     nextBtn.click();
+      //   } else if (start < currPosition) {
+      //     // slider.style.transform = "translateX(" + (-transitionSize+e.clientX-250) + "px)";
+      //     //slider.style.transform = "translateX(" + ((test*asd)-test+e.clientX) + "px)";
+      //     //console.log(-transitionSize- e.clientX-250);
+      //   }
+
+      slider.addEventListener("mouseleave", sliderMouseLeave);
+      slider.addEventListener("mouseup", sliderMouseUp);
+    }
+    function sliderClick() {
+      slider.style.cursor = "grab";
+      slider.removeEventListener("mousemove", sliderMouseMove);
+    }
+    function sliderMouseUp() {
+      pressed = false;
+      slider.style.cursor = "grab";
+
+      if (start > currPosition) {
+        moveSlide("next");
+      } else if (start < currPosition) {
+        moveSlide("prev");
+      }
+
+      slider.removeEventListener("mousemove", sliderMouseMove);
+      slider.removeEventListener("mouseleave", sliderMouseLeave);
+      slider.removeEventListener("mouseup", sliderMouseUp);
+    }
+    function sliderMouseLeave(e) {
+      pressed = false;
+      sliderMouseUp();
     }
   }
-  function arrowKeys(bool) {
-    if (bool) {
-      window.addEventListener("keydown", (e) => {
-        if (e.key === "ArrowLeft") {
-          prevButton.click();
-        } else if (e.key === "ArrowRight") {
-          nextButton.click();
-        }
-      });
+  function draggableTouchFunction() {
+    slider.addEventListener("touchstart", touchStart);
+    slider.addEventListener("touchmove", touchMove);
+    slider.addEventListener("touchend", touchEnd);
+
+    function touchStart(e) {
+      start = e.touches[0].clientX;
     }
-  }
-  function draggable(bool) {
-    if (bool) {
-      let pressed = false;
 
-      slider.addEventListener("click", sliderClick);
-      slider.addEventListener("mousedown", sliderMouseDown);
-      slider.addEventListener("mouseenter", sliderMouseEnter);
-
-      function sliderMouseEnter() {
-        slider.style.cursor = "grab";
-      }
-
-      function sliderMouseDown(e) {
-        pressed = true;
-        slider.style.cursor = "grabbing";
-        start = e.offsetX;
-        slider.addEventListener("mousemove", sliderMouseMove);
-      }
-
-      function sliderMouseMove(e) {
-        pressed = true;
-        slider.style.cursor = "grabbing";
-        e.preventDefault();
-        currPosition = e.offsetX;
-        if (start > 250) {
-          slider.style.transform =
-            "translateX(" +
-            (-imagesSizesArray[counter] * counter - 250 - (499 - e.clientX)) +
-            "px)";
-        } else {
-          slider.style.transform =
-            "translateX(" +
-            (-imagesSizesArray[counter] * counter + 250 - (499 - e.clientX)) +
-            "px)";
-        }
-
-        // slider.style.transform = "translateX(" + ((-imagesSizesArray[counter]*counter)-e.clientX-252+500) + "px)";
-        // slider.style.transform = "translateX(" + (asd-252-500) + "px)";
-        console.log(
-          -imagesSizesArray[counter] * counter - 250 - (499 - e.clientX)
-        );
-        console.log("Start ", start, " End ", currPosition);
-        //   if (start > currPosition) {
-        //     nextBtn.click();
-        //   } else if (start < currPosition) {
-        //     // slider.style.transform = "translateX(" + (-transitionSize+e.clientX-250) + "px)";
-        //     //slider.style.transform = "translateX(" + ((test*asd)-test+e.clientX) + "px)";
-        //     //console.log(-transitionSize- e.clientX-250);
-        //   }
-
-        slider.addEventListener("mouseleave", sliderMouseLeave);
-        slider.addEventListener("mouseup", sliderMouseUp);
-      }
-      function sliderClick() {
-        slider.style.cursor = "grab";
-        slider.removeEventListener("mousemove", sliderMouseMove);
-      }
-      function sliderMouseUp() {
-        pressed = false;
-        slider.style.cursor = "grab";
-
-        if (start > currPosition) {
-          nextButton.click();
-        } else if (start < currPosition) {
-          prevButton.click();
-        }
-
-        slider.removeEventListener("mousemove", sliderMouseMove);
-        slider.removeEventListener("mouseleave", sliderMouseLeave);
-        slider.removeEventListener("mouseup", sliderMouseUp);
-      }
-      function sliderMouseLeave(e) {
-        pressed = false;
-        sliderMouseUp();
+    function touchMove(e) {
+      currPosition = e.touches[0].clientX;
+      if (start > 470) {
+        slider.style.transform =
+          "translateX(" +
+          (-transitionSize + e.touches[0].screenX - 740) +
+          "px)";
+      } else if (start < 470) {
+        slider.style.transform =
+          "translateX(" +
+          (-transitionSize + e.touches[0].screenX - 250) +
+          "px)";
       }
     }
-  }
-  function draggableTouchFunction(bool) {
-    if (bool) {
-      slider.addEventListener("touchstart", touchStart);
-      slider.addEventListener("touchmove", touchMove);
-      slider.addEventListener("touchend", touchEnd);
 
-      function touchStart(e) {
-        start = e.touches[0].clientX;
-      }
-
-      function touchMove(e) {
-        currPosition = e.touches[0].clientX;
-        if (start > 470) {
-          slider.style.transform =
-            "translateX(" +
-            (-transitionSize + e.touches[0].screenX - 740) +
-            "px)";
-        } else if (start < 470) {
-          slider.style.transform =
-            "translateX(" +
-            (-transitionSize + e.touches[0].screenX - 250) +
-            "px)";
-        }
-      }
-
-      function touchEnd() {
-        if (start < currPosition) {
-          prevButton.click();
-        } else if (start > currPosition) {
-          nextButton.click();
-        }
+    function touchEnd() {
+      if (start < currPosition) {
+        moveSlide("prev");
+      } else if (start > currPosition) {
+        moveSlide("next");
       }
     }
   }
-  buttons(true);
-  dots(true);
-  arrowKeys(true);
-  draggable(true);
-  draggableTouchFunction(true);
+
+  // data attribute for initial slide
+  function dataSets() {
+    if (sliderContainer.dataset.button) {
+      buttons();
+    }
+    if (sliderContainer.dataset.dots) {
+      createNavDots(sliderContainer.dataset.startfromindex || 0);
+      goToSlide(sliderContainer.dataset.startfromindex || 0);
+    }
+    if (sliderContainer.dataset.arrowkeys) {
+      arrowKeys();
+    }
+    if (sliderContainer.dataset.draggable) {
+      draggable();
+    }
+    if (sliderContainer.dataset.draggabletouchfunction) {
+      draggableTouchFunction();
+    }
+  }
+
+
+  dataSets();
+  console.log(sliderContainer.attributes);
 }
 
 const sliders = document.querySelectorAll(".carousel-container");
@@ -268,3 +277,6 @@ const sliders = document.querySelectorAll(".carousel-container");
 sliders.forEach((slide) => {
   slider(slide);
 });
+
+//glitch on first and last el when move
+//goToSlide input
---
+++ shared.js
@@ -0,0 +1,14 @@
+// function DotGrey(){
+//     navigationDots.forEach((dot) => {
+//         if (dot.classList.contains("dot-red")) {
+//           dot.classList.remove("dot-red");
+//         }
+//       });
+// }
+
+
+
+export function test(user) {
+    alert(`Hello, ${user}!`);
+  }  // no ; at the end
+
